de5b54814ba4f71f67c57e67912f3974aac041ad
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-01-04 00:56:49 -0500

    Scoring function
    Fixes #1

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,16 @@
 """
 Utilities
 """
+
+import keras.backend as K
+
+
+def prediction_score(y_true, y_pred, temperature=1.0):
+    """
+    Return log score of predictions using provided temperature value
+    """
+
+    y_pred = K.log(y_pred) / temperature
+    y_pred = K.exp(y_pred) / K.sum(K.exp(y_pred), axis=-1, keepdims=True)
+
+    return K.categorical_crossentropy(y_pred, y_true)
