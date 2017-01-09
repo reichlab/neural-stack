c406e24c9799f02cdf248ea5be878265486838fa
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-01-08 19:21:06 -0500

    Log score to final point model

--- src/models.py
+++ src/models.py
@@ -21,11 +21,15 @@ class ModelMeta(abc.ABC):
         Use this to train the model
         """
 
+        pass
+
     @abc.abstractmethod
     def predict():
         """
         Use this to take out predictions
         """
+
+        pass
 
 
 class SimpleDense(ModelMeta):
@@ -46,6 +50,55 @@ class SimpleDense(ModelMeta):
         model.compile(loss="mse", optimizer="rmsprop")
 
         self.model = model
+
+    def fit(self, *args, **kwargs):
+        return self.model.fit(*args, **kwargs)
+
+    def predict(self, *args):
+        return self.model.predict(*args)
+
+
+class SimpleDenseDirect(ModelMeta):
+    """
+    Take log scores and week number to give actual point prediction directly
+    """
+
+    def __init__(self, n_models, week_embedding_size):
+        """
+        Parameters
+        ----------
+        n_models : int
+            Number of model log scores
+        week_embedding_size : int
+            Embedding vector size for week
+        """
+
+        log_scores = Sequential()
+
+        log_scores.add(Dense(10, input_dim=n_models))
+        log_scores.add(Activation("tanh"))
+        log_scores.add(Dense(week_embedding_size))
+
+        weeks = Sequential()
+        # Encoding all the weeks possible (not just the ones from 10 to 30)
+        weeks.add(Embedding(54, week_embedding_size, input_length=1))
+        weeks.add(Flatten())
+        weeks.add(Activation("tanh"))
+        weeks.add(Dense(10, W_regularizer=l2(0.01)))
+
+        merged = Sequential()
+        merged.add(Merge([log_scores, weeks], mode="concat", concat_axis=-1))
+        merged.add(Dense(20))
+        merged.add(Activation("relu"))
+        merged.add(Dense(10))
+        merged.add(Activation("relu"))
+        merged.add(Dense(1))
+
+        merged.compile(loss="mse", optimizer="rmsprop")
+
+        self.model = merged
+        self.n_models = n_models
+        self.week_embedding_size = week_embedding_size
 
     def fit(self, *args, **kwargs):
         return self.model.fit(*args, **kwargs)
