3946d477b8aa5582c7def82676011b583a0818b5
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-01-08 22:04:31 -0500

    Use packrat

--- scripts/collect-stacking-data.R
+++ scripts/collect-stacking-data.R
@@ -1,5 +1,9 @@
 # Collect stacking data in a csv file
 
+setwd(file.path(getwd(), "scripts"));
+source(".Rprofile");
+
+setwd("../");
 setwd(snakemake@input[[1]]);
 source(snakemake@config[["assembling-script"]]);
 
