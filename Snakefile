7e36df56826e54bfe34fedd4a2078fdc54844def
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-07-10 02:19:59 -0400

    Script to separate out data properly

--- Snakefile
+++ Snakefile
@@ -11,20 +11,21 @@ rule actual_data:
 # Use ensemble-data.csv to create separate prediction files for each
 # model (KDE, KCDE, SARIMA)
 
-MODELS = ["KDE", "KCDE", "SARIMA"]
+MODELS = ["kde", "kcde", "sarima"]
 
 rule separate_model_data:
     input:
-        ensemble_csv = "data/external/ensemble-data.csv"
+        ensemble_csv = "data/external/ensemble-data.csv",
+        out_dir = "data/processed/"
     output:
-        index = expand("data/processed/{model}/index.csv", model=MODELS)
-        scores = expand("data/processed/{model}/scores.csv", model=MODELS)
-        onset = expand("data/processed/{model}/onset.csv", model=MODELS)
-        peak_wk = expand("data/processed/{model}/peak_wk.csv", model=MODELS)
-        peak = expand("data/processed/{model}/peak.csv", model=MODELS)
-        one_wk = expand("data/processed/{model}/one_wk.csv", model=MODELS)
-        two_wks = expand("data/processed/{model}/two_wk.csv", model=MODELS)
-        three_wks = expand("data/processed/{model}/three_wk.csv", model=MODELS)
-        gour_wks = expand("data/processed/{model}/four_wk.csv", model=MODELS)
+        index = expand("data/processed/{model}/index.csv", model=MODELS),
+        scores = expand("data/processed/{model}/scores.np.gz", model=MODELS),
+        onset = expand("data/processed/{model}/onset.np.gz", model=MODELS),
+        peak_wk = expand("data/processed/{model}/peak_wk.np.gz", model=MODELS),
+        peak = expand("data/processed/{model}/peak.np.gz", model=MODELS),
+        one_wk = expand("data/processed/{model}/one_wk.np.gz", model=MODELS),
+        two_wks = expand("data/processed/{model}/two_wk.np.gz", model=MODELS),
+        three_wks = expand("data/processed/{model}/three_wk.np.gz", model=MODELS),
+        gour_wks = expand("data/processed/{model}/four_wk.np.gz", model=MODELS)
     script:
         "scripts/separate-model-data.py"
---
+++ scripts/separate-model-data.py
@@ -0,0 +1,76 @@
+"""
+Create model data files from ensemble-data.csv
+"""
+
+import numpy as np
+import os
+import pandas as pd
+import pymmwr
+
+
+# Read predictions
+df = pd.read_csv(snakemake.input.ensemble_csv, index_col=0)
+df = df.dropna()
+
+def get_epiweek(season: str, season_wk: int):
+    """
+    Get epiweek integer
+    """
+
+    season_first_year = int(season.split("/")[0])
+    season_second_year = int(season.split("/")[1])
+
+    # Week 31 is season_wk 01
+    week = 30 + season_wk
+    first_year_weeks = pymmwr.mmwr_weeks_in_year(season_first_year)
+
+    epiweek_year = season_first_year
+    if week > first_year_weeks:
+        week -= first_year_weeks
+        epiweek_year = season_second_year
+
+    return int(str(epiweek_year) + str(week).zfill(2))
+
+df["epiweek"] = list(map(get_epiweek, df["analysis_time_season"], df["analysis_time_season_week"]))
+df.drop(["analysis_time_season", "analysis_time_season_week"], axis=1, inplace=True)
+
+models = list(df["model"].unique())
+for model in models:
+    model_dir = os.path.join(snakemake.input.out_dir, model)
+    os.makedirs(model_dir, exist_ok=True)
+
+    df_sub = df[df["model"] == model]
+    identifiers = df_sub[["epiweek", "region"]]
+    identifiers.to_csv(os.path.join(model_dir, "index.csv"), index=False)
+
+    # Write log scores
+    # First 7 are
+    # 1. onset
+    # 2. peak week
+    # 3. peak incidence
+    # 4. week 1
+    # 5. week 2
+    # 6. week 3
+    # 7. week 4
+    # Next 7 are competition log scores for the same targets as above
+    np.savetxt(os.path.join(model_dir, "scores.np.gz"), df_sub.iloc[:, 0:14].values)
+
+    # Write onset bin values
+    # Bins go from season week 10 (epiweek 40) to season week 42 (epiweek
+    # depends on the number of weeks in first year) and a none bin
+    np.savetxt(os.path.join(model_dir, "onset.np.gz"), df_sub.iloc[:, 14:48].values)
+
+    # Write peak week bin values
+    # Bins go from season week 10 (epiweek 40) to season week 42
+    np.savetxt(os.path.join(model_dir, "peak_wk.np.gz"), df_sub.iloc[:, 48:81].values)
+
+    # Write peak incidence bin values
+    # Bins from 0.0 to 13.0 in steps of 0.1
+    np.savetxt(os.path.join(model_dir, "peak.np.gz"), df_sub.iloc[:, 81:212].values)
+
+    # Write week ahead prediction bins
+    start = 212
+    for idx, week in enumerate(["one", "two", "three", "four"]):
+        np.savetxt(os.path.join(model_dir, f"{week}_wk.np.gz"), df_sub.iloc[
+            :, start + (idx * 131): start + ((idx + 1) * 131)
+        ].values)
