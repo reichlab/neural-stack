0dfddc06fdd6a6471a7e01fb4ac79c7abca9932f
Abhinav Tushar <lepisma@fastmail.com>
2017-11-05 00:51:48 -0400

    Test for multiple dominant models

--- tests/test_models.py
+++ tests/test_models.py
@@ -59,3 +59,21 @@ def test_dem_dominant_model():
     expected_weights[0] = 1
 
     assert np.allclose(weights, expected_weights)
+
+
+def test_dem_multiple_dominant_models():
+    """
+    Test dem with multiple dominant models
+    """
+
+    N = 1000 # observations
+    M = 15   # models
+
+    scores = np.zeros((N, M)) + 0.001
+    scores[:, :5] = 0.999
+
+    weights = models.dem(scores)
+    expected_weights = np.zeros(M)
+    expected_weights[:5] = 0.2
+
+    assert np.allclose(weights, expected_weights)
