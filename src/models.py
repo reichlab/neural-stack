730628869f30f469c10f29fd68c90cc0e5b5b2de
Abhinav Tushar <lepisma@fastmail.com>
2017-11-21 02:11:27 -0500

    Remove old cnn model

--- src/models.py
+++ src/models.py
@@ -3,10 +3,6 @@ Models used in notebooks
 """
 
 import numpy as np
-from keras.layers import (Activation, Convolution1D, Convolution2D, Dense,
-                          Dropout, Embedding, Flatten, Merge)
-from keras.models import Sequential
-from keras.regularizers import l2
 
 
 def dem(mat, weights=None, epsilon=None):
@@ -52,63 +48,3 @@ def dem(mat, weights=None, epsilon=None):
             if (marginal_diff <= epsilon) or ((marginal_diff / -log_marginal) <= epsilon):
                 break
     return weights
-
-
-def conv1D_distribution(n_models,
-                        n_bins,
-                        week_embedding_size,
-                        week_embedding_matrix=None):
-    """
-    One dimensional conv model over input distribution to give an output
-    distribution
-
-    Merges two branches
-    - predictions : (batch_size, n_bins, n_models)
-    - weeks : (batch_size, 1)
-
-    Parameters
-    ----------
-    n_models : int
-        Number of models (predictive distributions)
-    n_bins : int
-        Number of bins in the prediction distribution
-    week_embedding_size : int
-        Embedding vector size for week
-    week_embedding_matrix : np.ndarray
-        Embedding matrix to use as initial weight
-    """
-
-    preds = Sequential()
-
-    preds.add(
-        Convolution1D(
-            64, 5, border_mode="same", input_shape=(n_bins, n_models)))
-
-    preds.add(Convolution1D(20, 5, border_mode="same"))
-    preds.add(Flatten())
-    preds.add(Dense(30, W_regularizer=l2(0.01)))
-    preds.add(Activation("tanh"))
-
-    weeks = Sequential()
-    # Encoding all the weeks possible (not just the ones from 10 to 30)
-    if week_embedding_matrix is not None:
-        weeks.add(
-            Embedding(
-                54,
-                week_embedding_matrix.shape[1],
-                input_length=1,
-                weights=[week_embedding_matrix]))
-    else:
-        weeks.add(Embedding(54, week_embedding_size, input_length=1))
-    weeks.add(Flatten())
-    weeks.add(Activation("tanh"))
-    weeks.add(Dense(10, W_regularizer=l2(0.01)))
-
-    merged = Sequential()
-    merged.add(Merge([preds, weeks], mode="concat", concat_axis=1))
-    merged.add(Dense(30, W_regularizer=l2(0.01)))
-    merged.add(Activation("relu"))
-    merged.add(Dense(n_bins))
-    merged.add(Activation("softmax"))
-
-    return merged
